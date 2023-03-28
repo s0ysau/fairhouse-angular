0d7dfc8b8adf96f2e18ed242f258f94c40c32baa
jerricklc@gmail.com <jerricklc@gmail.com>
2023-03-28 12:01:39 -0400

    first angular app - introduction

---
+++ src/app/housing-list/housing-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+
+
+@Component({
+  selector: 'app-housing-list',
+  templateUrl: './housing-list.component.html',
+  styleUrls: ['./housing-list.component.css']
+})
+export class HousingListComponent {
+
+  @Input() locationList: HousingLocation [] = [];
+  results: HousingLocation[] = [];
+  @Output() selectedLocationEvent = new EventEmitter<HousingLocation>();
+
+  searchHousingLocations (searchText: string){ 
+    if (!searchText) return;
+    this.results = this.locationList.filter( 
+      location => location.city.toLowerCase().includes(searchText.toLowerCase())
+      )
+  }
+  selectHousingLocation(location: HousingLocation){
+    this.selectedLocationEvent.emit(location)
+  }
+}
